3a0ff9d2e904ef9e7de012ec2978a63868bcffbf
mgsong19 <mg.song@kthd.co.kr>
2022-12-06 12:10:47 -0500

    12-10

--- medium/inplaceColorSort.py
+++ medium/inplaceColorSort.py
@@ -1,20 +1,39 @@
 class Solution(object):
-    def sortColors(self, nums):
+    # def sortColors(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: None Do not return anything, modify nums in-place instead.
+    #     """
+    #     start = 0
+    #     end = len(nums) - 1
+    #     i = 0
+    #     while(i < len(nums)-1):
+    #         if nums[i] == 0:
+    #             nums[start], nums[i] = nums[i], nums[start]
+    #             start += 1
+    #             i = start
+    #         elif nums[i] == 2 and end > i:
+    #             nums[end], nums[i] = nums[i], nums[end]
+    #             end -= 1
+    #         else:
+    #             i += 1
+    # slightly faster 
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        start = 0
-        end = len(nums) - 1
+        r = 0
+        b = len(nums) - 1
         i = 0
-        while(i < len(nums)-1):
+        while i <= b:
             if nums[i] == 0:
-                nums[start], nums[i] = nums[i], nums[start]
-                start += 1
-                i = start
-            elif nums[i] == 2 and end > i:
-                nums[end], nums[i] = nums[i], nums[end]
-                end -= 1
+                nums[r], nums[i] = nums[i], nums[r]
+                r += 1
+                if nums[i] == 0:
+                    i += 1
+            elif nums[i] == 2:
+                nums[b], nums[i] = nums[i], nums[b]
+                b -= 1
             else:
                 i += 1
 
